6f3ac36124cfbf6eeaed9b6853be78dea4493e84
Nemanja Malesija <malesija13@gmail.com>
2023-02-06 15:29:04 +0100

    setup initial types and initial state, install react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
+type quizState = {
+  numberOfQuestions: number;
+  amount: number;
+  category: number;
+  difficulty: string;
+  type: string;
+};
+
+type quizCategories = {
+  sports: number; // 21
+  geography: number; // 22
+  mythology: number; // 20
+  art: number; // 25
+};
+
+const initialState: quizState = {
+  numberOfQuestions: 10,
+  amount: 10,
+  category: 21,
+  difficulty: 'easy',
+  type: 'multiple',
+};
+
+const categories = {
+  sports: 21,
+  geography: 22,
+  mythology: 20,
+  art: 25,
+};
+
 function App() {
+  const [quiz, setQuiz] = useState(initialState);
+
   return <div className="App"></div>;
 }
 
