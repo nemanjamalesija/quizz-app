f6a75d6068f7f51440576d40901e7b8d9b41cbe1
Nemanja Malesija <malesija13@gmail.com>
2023-02-07 14:20:55 +0100

    Round answered questions percentage, add out of out stats

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -4,14 +4,16 @@ import { quizScoreTrackerProps } from '../types/types';
 const GameOverModal = (props: quizScoreTrackerProps) => {
   const { correctAnswers, currentQuestionIndex } = props;
 
-  const corectAnsweredPercentage =
-    (correctAnswers / (currentQuestionIndex + 1)) * 100;
+  const corectAnsweredPercentage = Math.round(
+    (correctAnswers / (currentQuestionIndex + 1)) * 100
+  );
 
   return (
     <div className="modal">
       <h2 className="heading-modal">Game Over!</h2>
       <p className="modal-paragraph">
-        You answered {corectAnsweredPercentage}% questions correctly
+        You answered {correctAnswers} / {currentQuestionIndex + 1} or{' '}
+        {corectAnsweredPercentage}% questions correctly
       </p>
     </div>
   );
